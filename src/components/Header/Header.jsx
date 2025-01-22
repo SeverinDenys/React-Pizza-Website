1755c8e8dccf3e856b33d76aaf5af5d2b85d35bf
Denys Severin <severindenys367@gmail.com>
2025-01-22 16:51:28 +0100

    added header section mobile and styles to it

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import PizzaLogoIcon from "../../assets/icons/pizza-logo.svg";
+import { ShoppingCart } from "lucide-react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="container">
+        <div className="header__logo">
+          <img width="38" src={PizzaLogoIcon} alt="Pizza logo" />
+          <div>
+            <h1>React Pizza</h1>
+            <p>The most delicious Pizza in Universe</p>
+          </div>
+        </div>
+        <div className="header__cart">
+          <a href="/cart.html" className="button button--cart">
+            <span>20 $</span>
+            <div className="button__delimiter"></div>
+            <ShoppingCart size={30} />
+            <span>3</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
