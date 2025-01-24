f0c3835ad92175fffc4bcfbe781f391aa2a542aa
Denys Severin <severindenys367@gmail.com>
2025-01-24 11:38:54 +0100

    added js module with pizza data and mapped through it in pizza block jsx

--- src/components/PizzaBlock/PizzaBlock.jsx
+++ src/components/PizzaBlock/PizzaBlock.jsx
@@ -1,35 +1,36 @@
 import { Plus } from "lucide-react";
+import {pizzaData} from '../../../pizzaInfo'
 
 const PizzaBlock = () => {
   return (
-    <div className="pizza-block">
-      <img
-        className="pizza-block__image"
-        src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
-        alt="Pizza"
-      />
-      <h4 className="pizza-block__title">Pizza</h4>
-      <div className="pizza-block__selector">
-        <ul>
-          <li className="active">thin</li>
-          <li>traditional</li>
-        </ul>
-        <ul>
-          <li className="active">26 cm.</li>
-          <li>30 cm.</li>
-          <li>40 cm.</li>
-        </ul>
-      </div>
-      <div className="pizza-block__bottom">
-        <div className="pizza-block__price">from 15 $</div>
-        <div className="button button--outline button--add">
-          <Plus />
-          <span>Add</span>
-          <i className="added-amount">2</i>
+    <>
+      {pizzaData.map((pizza, index) => (
+        <div key={index} className="pizza-block">
+          <img className="pizza-block__image" src={pizza.img} alt="Pizza" />
+          <h4 className="pizza-block__title">{pizza.name}</h4>
+          <div className="pizza-block__selector">
+            <ul>
+              <li className="active">{pizza.firstSizeName}</li>
+              <li>{pizza.secondSizeName}</li>
+            </ul>
+            <ul>
+              <li className="active">{pizza.firstPortionSize}</li>
+              <li>{pizza.secondPortionSize}</li>
+              <li>{pizza.thirdPortionSize}</li>
+            </ul>
+          </div>
+          <div className="pizza-block__bottom">
+            <div className="pizza-block__price">from {pizza.price} $</div>
+            <div className="button button--outline button--add">
+              <Plus />
+              <span>Add</span>
+              <i className="added-amount">2</i>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      ))}
+    </>
   );
 };
 
-export default PizzaBlock;
+export default PizzaBlock
