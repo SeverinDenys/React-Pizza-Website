b5b5859a7a6a2492c390d7d236d2563187af0249
Denys Severin <severindenys367@gmail.com>
2025-02-03 14:15:01 +0100

    added state for sort. setPopUpVisibility

--- src/components/Sort/Sort.jsx
+++ src/components/Sort/Sort.jsx
@@ -1,20 +1,44 @@
+import { useState } from "react";
 import arrowUpIcon from "../../assets/icons/arrow-up.svg";
 
 const Sort = () => {
+  const [isVisiblePopUp, setIsVisiblePopUp] = useState(false);
+  const [selectedSort, setSelectedSort] = useState(0);
+
+  const handlePopUp = () => {
+    setIsVisiblePopUp(!isVisiblePopUp);
+  };
+
+  const handleSelectedSort = (index) => {
+    setSelectedSort(index);
+    setIsVisiblePopUp(false);
+  };
+
+  const list = ["popularity", "price", "alphabet"];
+  const sortName = list[selectedSort];
+
   return (
     <div className="sort">
       <div className="sort__label">
         <img src={arrowUpIcon} alt="arrow icon up" />
         <b>sort by:</b>
-        <span>popularity</span>
+        <span onClick={handlePopUp}>{sortName}</span>
       </div>
-      <div className="sort__popup">
-        <ul>
-          <li className="active">popularity</li>
-          <li>price</li>
-          <li>alphabet</li>
-        </ul>
-      </div>
+      {isVisiblePopUp && (
+        <div className="sort__popup">
+          <ul>
+            {list.map((listName, index) => (
+              <li
+                key={index}
+                className={selectedSort === index ? "active" : ""}
+                onClick={() => handleSelectedSort(index)}
+              >
+                {listName}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
